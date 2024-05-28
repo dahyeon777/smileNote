5f65dc418d11fcb0d8d0f1782174853085963137
dahyeon777 <daaaa0727@gmail.com>
2024-05-28 16:02:33 +0900

    초기 레이아웃

---
+++ app/src/main/java/com/dada/rootnote/WriteActivity.kt
@@ -0,0 +1,18 @@
+package com.dada.rootnote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.dada.rootnote.databinding.ActivityWriteBinding
+
+class WriteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWriteBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_write)
+
+
+    }
+}
